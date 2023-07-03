48298e89231102f9d359c4c5a7887e346fd53c17
2021060518 <104888345+2021060518@users.noreply.github.com>
2023-07-03 14:39:20 +0800

    Update app.config.ts
    更新备案信息

--- app.config.ts
+++ app.config.ts
@@ -18,12 +18,12 @@ export default defineAppConfig({
     },
     footer: {
       credits: {
-        enabled: false, // possible value are : true | false
-        repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
+        enabled: true, // possible value are : true | false
+        repository: 'https://beian.miit.gov.cn/' // our github repository
       },
-      navigation: false, // possible value are : true | false
+      navigation: true, // possible value are : true | false
       alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
-      message: '' // string that will be displayed in the footer (leave empty or delete to disable)
+      message: '浙ICP备18035094号-1' // string that will be displayed in the footer (leave empty or delete to disable)
     },
     // socials: {
     //   twitter: 'nuxtlabs',
