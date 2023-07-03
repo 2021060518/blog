4a53c8de1c553368adf8b05e7857a4671ad85462
witque <witque888@gmail.com>
2023-07-03 16:01:31 +0800

    页脚样式调整

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -5,8 +5,8 @@
         <meta name="referrer" content="no-referrer" />
       </Head>
       <ContentDoc />
-      <div style="text-align: center;">
-        <a href="https://beian.miit.gov.cn/">浙ICP备18035094号-1</a>
+      <div style="text-align: center; margin-top: 20%;">
+        <a href="https://beian.miit.gov.cn/" target="_blank">浙ICP备18035094号-1</a>
       </div>
     </main>
 </template>
